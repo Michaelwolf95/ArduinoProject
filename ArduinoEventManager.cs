f65e313860d3cb0b0a829b477e01b7e8a4fd1190
Michaelwolf95 <michaelwolf95@gmail.com>
2015-11-01 13:49:09 -0800

    EventManager

---
+++ ArduinoEventManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using SendAndReceive;
+
+public class ArduinoEventManager : MonoBehaviour
+{
+    public delegate void button1_Action();
+    public static event button1_Action OnPressed;
+    private SendAndReceive.SendAndReceive SR_Object;
+
+    public delegate void _update();
+    public static event _update ardUpdate;
+
+    // Use this for initialization
+    void Start ()
+    {
+        SR_Object = new SendAndReceive.SendAndReceive(); 
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    //if(SR_Object.GetArguments() == "1") //If Argument is _____
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if (OnPressed != null)
+            {
+                OnPressed();
+            }
+        }
+	}
+}
